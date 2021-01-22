ce3f6e84e2418fe0e174e61b4784c9688ef51b9f
imdadulhaque1 <imdadulhaqueimdad820@gmail.com>
2021-01-22 21:57:14 +0600

    Second Commit

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
+console.log("Hello World !")
 console.log("I am from index file !");
+
+
+
+
+/*
+    1. initilized as git:   git init
+    2. show the untracked and tracked file: git status
+    3. For making tracked file(single file) use the command:    git add test.txt
+    4. for making tracking all file: git add .
+
+    Note: After make sure the files as tracked, all the file are ready to commited
+
+    5. for commit: git commit -m "first Commit"
+    Note: git commit "comment: -m 'first Commit' "
+    6. Again theck status(Is master / is main): git status
+    7. For checking commit: git log  (show all information)
+                                    OR, git log --oneline (show needed information)
+
+*/
